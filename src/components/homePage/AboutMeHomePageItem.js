6782c78ac129a9bef0c666cac050e4ada1fee1b8
BramdeGooijer <degooijerbram@gmail.com>
2024-06-18 15:59:42 +0200

    Added some text and fonts for the about me home page item

--- src/components/homePage/AboutMeHomePageItem.js
+++ src/components/homePage/AboutMeHomePageItem.js
@@ -3,9 +3,12 @@ import { StyleSheet, Text } from "react-native";
 import LinearGradient from "expo";
 
 export default function AboutMeHomePageItem() {
+    const age = Math.floor((new Date() - new Date("2004-01-26")) / (1000 * 60 * 60 * 24 * 365.25));
+
     return (
         <View style={styles.container}>
             <Text style={styles.titleText}>Over mij</Text>
+            <Text style={styles.subText}>Hi, ik ben Bram, ik ben {age} jaar en dit is mijn portfolio. Wil je meer over mij weten? Klik hier...</Text>
         </View>
     )
 }
@@ -20,5 +23,10 @@ const styles = StyleSheet.create({
     },
     titleText: {
         color: 'white',
+        fontSize: 18,
+        fontFamily: 'Prompt-Bold',
+    },
+    subText: {
+
     }
 })
